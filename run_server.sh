5b174e010ee69e1d6f92b6fa5fbc68692f796aa0
Adam Kara <kara.p.adam@gmail.com>
2020-05-19 12:58:33 +0100

    Slight modifications on run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -2,13 +2,16 @@
 
 # Make sure to run this prior to the first run: sudo modprobe af_key
 
+image=l2tp-ipsec-vpn-server
+
+docker rm -f $image
+
 docker run \
-    --name l2tp-ipsec-vpn-server \
+    --name $image \
     --env-file ./.env_vpn \
     -p 500:500/udp \
     -p 4500:4500/udp \
     -v /lib/modules:/lib/modules:ro \
     -d --privileged \
-    --restart=unless-stopped \
-    --rm \
+    --restart=always \
     mirrmurr/l2tp-ipsec-vpn-server
